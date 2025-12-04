16dfba8f5116f3f98def7ae6ee857513008f6d89
Ken Morante <kenimorante@gmail.com>
2025-12-04 12:43:13 +0800

    updated login

--- frontend/src/AuthForm.js
+++ frontend/src/AuthForm.js
@@ -293,7 +293,7 @@ const AuthForm = ({ setCurrentPage, setIsAuthenticated, setUserRole, setCart, se
               disabled={loading}
               aria-label={showPassword ? 'Hide password' : 'Show password'}
             >
-              {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
+              {showPassword ? <Eye size={20} /> : <EyeOff size={20} />}
             </button>
           </div>
           {!isLogin && feedback && (
